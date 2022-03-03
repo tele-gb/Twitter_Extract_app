6a204bae1588e61ed4eee40ad84bd2d94787511b
tele-gb <kryz.wosik@gmail.com>
2022-03-03 18:23:08 +0000

    deleted few files

--- bq.py
+++ bq.py
@@ -7,19 +7,15 @@ def query_stackoverflow():
     query_job = client.query(
         """
         SELECT
-          CONCAT(
-            'https://stackoverflow.com/questions/',
-            CAST(id as STRING)) as url,
-          view_count
-        FROM `bigquery-public-data.stackoverflow.posts_questions`
-        WHERE tags like '%google-bigquery%'
-        ORDER BY view_count DESC
+        *
+        FROM `twitter-bank-sentiment.twitter_bank_sent.tweets`
+        WHERE upper(text) like '%@HSBC%'
         LIMIT 10"""
     )
 
     results = query_job.result()  # Waits for job to complete.
 
     for row in results:
-        print("{} : {} views".format(row.url, row.view_count))
+        print(row)
 
 query_stackoverflow()
