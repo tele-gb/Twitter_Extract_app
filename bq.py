7d3882d6f95fe3a3b364650ba7ef7a3d414c8ca6
tele-gb <kryz.wosik@gmail.com>
2022-03-11 11:58:40 +0000

    change

--- bq.py
+++ bq.py
@@ -43,9 +43,37 @@ dataframe = (
 )
 print(dataframe.head())
 
-dataframe.to_csv('bq_test.csv',sep=",",index=False,encoding='utf-8',quotechar="'")
+# dataframe.to_csv('bq_test.csv',sep=",",index=False,encoding='utf-8',quotechar="'")/
+dataframe.to_parquet('bq_test.gzip',compression="gzip")
 
+def append_data_from_para(bq_client, dataset, table_name, file_path, file_name):
+    """
+    Ingest data to BQ table from CSV file
+    """
 
+    dataset_ref = bq_client.dataset(dataset)
+    table_ref = dataset_ref.table(table_name)
+    
+    # try:
+    job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.PARQUET)
+    # job_config.source_format = bigquery.SourceFormat.
+    # job_config.skip_leading_rows = 1  # header skipped by default for append
+    # job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND  # append data to table
+    # job_config.allow_quoted_newlines = True  
+    # job_config.ignoreUnknownValues = True
+    # job_config.autodetect = True
+
+    full_file_path = os.path.join(file_path, file_name)
+    with open(full_file_path, "rb") as source_file:
+        job = bq_client.load_table_from_file(source_file, table_ref, job_config=job_config)
+
+    job.result()  # Waits for table load to complete.
+    #log.info("Loaded {} rows into {}:{}.".format(job.output_rows, dataset, table_name))
+    
+    # except Exception as Ex:
+    #     pass
+
+# append_data_from_CSV(client,"twitter_bank_sent","tweets2","./","bq_test.csv")
 
 
 def append_data_from_CSV(bq_client, dataset, table_name, file_path, file_name):
@@ -76,7 +104,8 @@ def append_data_from_CSV(bq_client, dataset, table_name, file_path, file_name):
     # except Exception as Ex:
     #     pass
 
-append_data_from_CSV(client,"twitter_bank_sent","tweets2","./","bq_test.csv")
+# append_data_from_CSV(client,"twitter_bank_sent","tweets2","./","bq_test.csv")
+append_data_from_para(client,"twitter_bank_sent","tweets2","./","bq_test.gzip")
 
 
 
