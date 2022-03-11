5bf6b1be898920d899d110c09994b835d18676a1
tele-gb <kryz.wosik@gmail.com>
2022-03-11 09:53:40 +0000

    updated bq and training data

--- bq.py
+++ bq.py
@@ -19,3 +19,21 @@ def query_stackoverflow():
         print(row)
 
 query_stackoverflow()
+
+def max_tweet():
+    client = bigquery.Client()
+    query_job = client.query(
+        """
+        SELECT
+        max(tweet_id)
+        FROM `twitter-bank-sentiment.twitter_bank_sent.tweets`
+        WHERE upper(text) like '%@HSBC%'
+        LIMIT 10"""
+    )
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        print(row)
+
+max_tweet()
