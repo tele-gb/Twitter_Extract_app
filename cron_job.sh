5bbe2257d0ef93925bc1c892f2b33a515535631d
tele-gb <kryz.wosik@gmail.com>
2022-03-12 19:25:24 +0000

    change

--- cron_job.sh
+++ cron_job.sh
@@ -1,1 +1,1 @@
-/home/kryz_wosik/Twitter_extract_app/env/bin/python3 /home/kryz_wosik/Twitter_Extract_app/twitter_bq.py
+/home/kryz_wosik/Twitter_extract_app/venv/bin/python3 /home/kryz_wosik/Twitter_Extract_app/twitter_bq.py
